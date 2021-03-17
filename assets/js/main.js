c987d00b227fd69019c252e39af66b3d8c5f7a4e
pwang86 <pwang86@gmail.com>
2021-03-18 09:55:02 +1100

    add json data and display bag functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,68 @@ tabEl.addEventListener('shown.bs.tab', function (e) {
   }
 });
 */
+
+const jsonData = `
+{
+  "bags" : [ 
+    {"url":"/bag/b1.jpg","name":"white plastic bag","qty":"500"},
+    {"url":"/bag/b2.jpg","name":"white plastic bag(Mercusys)","qty":"500"},
+    {"url":"/bag/b3.jpg","name":"Non-woven Bag","qty":"500"},
+    {"url":"/bag.b4.jpg","name":"Laptop bag","qty":"500"}
+  ],
+  "posters": [
+    {"url":"/poster/p1.jpg","name":"Deco M5","qty":"20"},
+    {"url":"/poster/p2.jpg","name":"Whole Home Mesh Wi-Fi Plastic Banner2 M5(3-pack)","qty":"20"},
+    {"url":"/poster/p3.jpg","name":"Tp-Link Logo Plastic Banner","qty":"20"},
+    {"url":"/poster/p4.jpg","name":"RE305 & RE650","qty":"20"}
+  ]
+}`;
+
+const data = JSON.parse(jsonData);
+function displayBags() {
+  const container = document.getElementById("bagContainer");
+  /*
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">White Plastic Bag</h5>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Qty:</small>
+      </div>
+    </div>
+  </div>
+  */ 
+  data.bags.map(item => {
+    const column = document.createElement("div");
+    column.setAttribute("class", "col"); 
+
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    const image = document.createElement("img");
+    image.src = "./assets/images" + item.url;
+    image.setAttribute("class", "card-img-top");
+    image.alt = "bag";
+
+    const body = document.createElement("div");
+    body.setAttribute("class", "card-body");
+    const title = document.createElement("h5");
+    title.setAttribute("class", "card-title");
+    title.innerHTML = item.name;
+    body.appendChild(title);
+
+    const footer = document.createElement("div");
+    footer.setAttribute("class", "card-footer");
+    const text = document.createElement("small");
+    text.setAttribute("class", "text-muted");
+    text.innerHTML = "Qty: " + item.qty;
+    footer.appendChild(text);
+
+    card.innerHTML = image.outerHTML + body.outerHTML + footer.outerHTML;
+    column.appendChild(card);
+    container.appendChild(column);
+    });
+  }
+  window.onload = displayBags;
