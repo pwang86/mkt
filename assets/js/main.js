02e7e515d9091c6c8f6f5c3c8b661afbc4297493
pwang86 <pwang86@gmail.com>
2021-03-19 16:21:21 +1100

    add displaybox and put serveral funcs into onload

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,6 @@ function showLastModified() {
   }
   document.getElementById('lastModified').innerHTML = res;
 }
-window.onload = showLastModified;
 
 // Before bootstrap 5.0, add following js to make dropdown menu work
 /*
@@ -37,11 +36,34 @@ tabEl.addEventListener('shown.bs.tab', function (e) {
 
 const jsonData = `
 {
-  "bags" : [ 
-    {"url":"/bag/b1.jpg","name":"white plastic bag","qty":"500"},
-    {"url":"/bag/b2.jpg","name":"white plastic bag(Mercusys)","qty":"500"},
-    {"url":"/bag/b3.jpg","name":"Non-woven Bag","qty":"500"},
-    {"url":"/bag.b4.jpg","name":"Laptop bag","qty":"500"}
+  "bags": [ 
+    {"url": "/bag/b1.jpg", "name": "white plastic bag", "qty": "500"},
+    {"url": "/bag/b2.jpg", "name": "white plastic bag(Mercusys)", "qty": "500"},
+    {"url": "/bag/b3.jpg", "name": "Non-woven Bag", "qty": "500"},
+    {"url": "/bag.b4.jpg", "name": "Laptop bag", "qty": "500"}
+  ],
+  "boxes": [
+    {"name": "HS110", "qty": "313"},
+    {"name": "AX10", "qty": "11"},
+    {"name": "Ax20", "qty": "10"},
+    {"name": "VR2100", "qty": "10"},
+    {"name": "PA8033PKIT", "qty": "10"},
+    {"name": "RE505X", "qty": "10"},
+    {"name": "TX3000E", "qty": "10"},
+    {"name": "WR902AC", "qty": "12"},
+    {"name": "KL110", "qty": "25"},
+    {"name": "KL110B", "qty": "19"},
+    {"name": "KL130", "qty": "31"},
+    {"name": "KL130B", "qty": "23"},
+    {"name": "KP105", "qty": "200"},
+    {"name": "KP115", "qty": "200"},
+    {"name": "M4 (2 Pack)", "qty": "95"},
+    {"name": "M5 (3 Pack)", "qty": "56"},
+    {"name": "RE300", "qty": "1"},
+    {"name": "PA9020PKIT", "qty": "23"},
+    {"name": "W9960", "qty": "2"},
+    {"name": "WA850RE", "qty": "54"},
+    {"name": "C20", "qty": "194"}
   ],
   "posters": [
     {"url":"/poster/p1.jpg","name":"Deco M5","qty":"20"},
@@ -50,23 +72,24 @@ const jsonData = `
     {"url":"/poster/p4.jpg","name":"RE305 & RE650","qty":"20"}
   ]
 }`;
-
 const data = JSON.parse(jsonData);
+
+// display all bags
 function displayBags() {
   const container = document.getElementById("bagContainer");
   /*
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">White Plastic Bag</h5>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Qty:</small>
+    <div class="col">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">White Plastic Bag</h5>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Qty:</small>
+        </div>
       </div>
     </div>
-  </div>
-  */ 
+  */
   data.bags.map(item => {
     const column = document.createElement("div");
     column.setAttribute("class", "col"); 
@@ -98,4 +121,36 @@ function displayBags() {
     container.appendChild(column);
     });
   }
-  window.onload = displayBags;
+
+  // display all dummy boxes
+  function displayBoxes() {
+    const container = document.getElementById("boxContainer");
+    /*
+      <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          HS110
+          <span class="badge bg-custom rounded-pill">313</span>
+        </li>
+      </ul>
+    */
+    data.boxes.map(item => {
+      const unorderedList = document.createElement("ul");
+      unorderedList.setAttribute("class", "list-group");
+
+      const listItem = document.createElement("li");
+      listItem.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center");
+      listItem.innerHTML = item.name;
+      
+      const span = document.createElement("span");
+      span.setAttribute("class", "badge bg-custom rounded-pill");
+      span.innerHTML = item.qty;
+      listItem.appendChild(span);
+      unorderedList.appendChild(listItem);
+      container.appendChild(unorderedList);
+    });
+  };
+  window.onload = function() {
+    showLastModified();
+    displayBags();
+    displayBoxes();
+  }
